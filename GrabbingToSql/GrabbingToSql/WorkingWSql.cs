2751ccc20c8c997947e6db8b1c417bcf9340ff84
yuk <Common@HSHSERMITN>
2017-02-04 12:22:26 +0200

    done1

--- GrabbingToSql/GrabbingToSql/WorkingWSql.cs
+++ GrabbingToSql/GrabbingToSql/WorkingWSql.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace GrabbingToSql
 {
@@ -11,10 +12,28 @@ namespace GrabbingToSql
     {
         private string CurrCompId;
         private SqlConnection Connection;
-        public WorkingWSql(string ConnectionStr)
+        public WorkingWSql(string userid, string password, string Server, string Database)
         {
+            SqlConnectionStringBuilder builder =
+            new SqlConnectionStringBuilder();
+            builder["user id"] = userid;
+            builder["password"] = password;
+            builder["Server"] = Server;
+            builder["Database"] = Database;
+            builder["Connect Timeout"] = 1000;
+
+            string ConnectionStr = builder.ConnectionString;
+
             Connection = new SqlConnection(ConnectionStr);
-            string QueryCreateTable1 = @"CREATE TABLE IF NOT EXISTS OverView (id INT ";
+            string QueryCreateDatabase = $"CREATE DATABASE IF NOT EXISTS '{Database}'";
+            string QueryCreateTable1 = @"CREATE TABLE IF NOT EXISTS 'OverView' (
+            'id' INT, 
+            'RegisteredOfficeAddress' VARCHAR(255),
+            PRIMARY KEY ('id')";
+
+            SqlCommand Command = new SqlCommand(QueryCreateTable1, Connection);
+            Command.Connection.Open();
+            MessageBox.Show(Command.ExecuteScalar().ToString());
         }
     }
 }
