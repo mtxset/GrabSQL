5c30847b468842fe52bfd7ff459079745151f975
yuk <Common@HSHSERMITN>
2017-02-04 15:22:44 +0200

    Create database and table

--- GrabbingToSql/TestR/Form1.cs
+++ GrabbingToSql/TestR/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GrabbingToSql;
 
 namespace TestR
 {
@@ -15,6 +16,12 @@ namespace TestR
         public Form1()
         {
             InitializeComponent();
+            WorkingWSql SqlObj = new WorkingWSql("yuk","yurak123","192.168.0.103", "companieshouse");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
