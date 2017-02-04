89a4e7b45d25a52161b6a6f46a3bf12ce2ce4f01
Romanas <gensatus@gmail.com>
2017-02-04 10:21:39 +0200

    parser class

---
+++ GrabbingToSql/GrabbingToSql/Parser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrabbingToSql
+{
+    class Parser
+    {
+        class HTTPConnector
+        {
+            private HttpWebRequest httpRequest;
+
+            private void Init()
+            {
+                httpRequest.KeepAlive = false;
+            }
+
+            public HTTPConnector()
+            {
+                
+            }
+
+            public HTTPConnector( string initialWeb )
+            {
+
+            }
+            
+            public 
+        }
+
+
+    }
+}
