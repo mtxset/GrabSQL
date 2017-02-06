c59ee8c428cb7b91bfee95255f4814362f11f507
yuk <Common@HSHSERMITN>
2017-02-06 15:53:51 +0200

    f3

--- GrabbingToSql/GrabbingToSql/WorkingWSql.cs
+++ GrabbingToSql/GrabbingToSql/WorkingWSql.cs
@@ -12,22 +12,52 @@ namespace GrabbingToSql
         public WorkingWSql(string userid, string password, string Server, string Database)
         {
             Parser.ConfigLoader cLoader = new Parser.ConfigLoader();
-            Dictionary<string, string> tDic = cLoader.LoadFields(Parser.PageTab.Overview);
-            string[] FiNamesOverView = tDic.Values.ToArray();
+            Dictionary<string, string> tDicOverView = cLoader.LoadFields(Parser.PageTab.Overview);
+            string[] FiNamesOverView = tDicOverView.Values.ToArray();
 
-            InitializeDB(userid, password, Server, Database, FiNamesOverView);
+            Dictionary<string, string> tDicPeople = cLoader.LoadFields(Parser.PageTab.People);
+            string[] FiNamesPeople = tDicPeople.Values.ToArray();
+
+            Dictionary<string, string> tDicFillingHistory = cLoader.LoadFields(Parser.PageTab.FillingHistory);
+            string[] FiNamesFillingHistory = tDicFillingHistory.Values.ToArray();
+
+            InitializeDB(userid, password, Server, Database);
+            InitializeTables(FiNamesOverView, "OverView");
+            InitializeTables(FiNamesPeople, "People");
+            InitializeTables(FiNamesPeople, "FillighHistory");
         }
 
-        private bool InitializeDB(string userid, string password, string Server, string Database, string[] FiNamesOverView)
+        private bool InitializeTables(string[] FiNames, string TableName)
+        {
+            string QueryCreateTable1 = $@"CREATE TABLE IF NOT EXISTS {TableName} (
+id INT NOT NULL,
+idComp INT NOT NULL,
+";
+            foreach (string FiName in FiNames)
+            {
+                QueryCreateTable1 += $"{FiName} VARCHAR(255),\r\n";
+            }
+            QueryCreateTable1 += @"PRIMARY KEY (id), INDEX idCompInd (idComp))";
+            try
+            {
+                if (!OpenConnection()) return false;
+                MySqlCommand Command = new MySqlCommand(QueryCreateTable1, Connection);
+                Command.ExecuteNonQuery();
+                if (!CloseConnection()) return false;
+
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Error initializing tables");
+                return false;
+            }
+        }
+
+        private bool InitializeDB(string userid, string password, string Server, string Database)
         {
             string QueryCreateDatabase = $"CREATE DATABASE IF NOT EXISTS {Database}";
-            string QueryCreateTable1 = @"CREATE TABLE IF NOT EXISTS OverView (
-                id INT NOT NULL,";
-                foreach (string FiName in FiNamesOverView)
-                {
-                    QueryCreateTable1 += $"{FiName} VARCHAR(255),";
-                }
-                QueryCreateTable1 += "PRIMARY KEY (id))";
+            
             try
             {
                 MySqlConnectionStringBuilder builder =
@@ -50,11 +80,6 @@ namespace GrabbingToSql
                 builder["Database"] = Database;
                 ConnectionStr = builder.ConnectionString;
                 Connection = new MySqlConnection(ConnectionStr);
-
-                if (!OpenConnection()) return false;                
-                Command = new MySqlCommand(QueryCreateTable1, Connection);                
-                Command.ExecuteNonQuery();
-                if (!CloseConnection()) return false;
 
                 return true;
             }
